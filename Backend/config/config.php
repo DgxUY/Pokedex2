7d411429e05b92ed67eaa0c741d380d56e43ac94
Diego Villagran <dieguvillagran@.com>
2025-09-20 19:57:42 -0300

    Test Commit

--- Backend/config/config.php
+++ Backend/config/config.php
@@ -1,17 +1,10 @@
 <?php
 
-// constants
+// constants 
 
 const POKEAPI_URL = "https://pokeapi.co/api/v2/";
 const DEFAULT_LIMIT = 20;
 const MAX_LIMIT = 300;
 
-const DEBUG_MODE = true;
-
-function debug($message) {
-    if (DEBUG_MODE) {
-        error_log("DEBUG: " . print_r($message, true));
-    }
-}
 
 ?>
