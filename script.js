5e0c6c50b9ff793e15da0da900868dec66080c05
Diego Villagran <dieguvillagran@gmail.com>
2025-09-26 14:55:10 -0300

    Modificaciones en script

--- script.js
+++ script.js
@@ -3,89 +3,78 @@ const searchButton = document.getElementById("search");
 const resultDiv = document.getElementById("result");
 const generationInput = document.getElementById("generation");
 const searchButton2 = document.getElementById("search2");
-const resultDiv2 = document.getElementById("result2");
-const abilityInput = document.getElementById("ability");
-const searchButton3 = document.getElementById("search3");
-const resultDiv3 = document.getElementById("result3");
-const loadMoreButton = document.getElementById("load-more");
+const typeInput = document.getElementById("type");
+const searchButton4 = document.getElementById("search4");
+let allPokemonHTML = document.getElementById("result");
 
 
+resultDiv.style.width = "100%";
+resultDiv.style.height = "100%";
+resultDiv.style.display = "flex";
+resultDiv.style.flexWrap = "wrap";
+resultDiv.style.justifyContent = "center";
+resultDiv.style.alignItems = "center";
+resultDiv.style.gap = "20px";
 
-searchButton.addEventListener("click", () => {
-  const pokemonName = pokemonInput.value;
-  fetch(`/api/pokemon/${pokemonName}`)
-    .then((response) => response.json())
-    .then((result) => {
-      resultDiv.innerHTML = `
-         <p>${result.data.name}</p>
-         <p>${result.data.types.map((type) => type.type.name).join(", ")}</p>
-         <img src="${result.data.sprites.front_default}"></img>
-         <button id="See details">See details</button>
-         `;
-    })
-    .catch((error) => {
-      console.error("No se encontro el pokemon");
-    });
-});
-
-searchButton2.addEventListener("click", () => {
-  const generation = generationInput.value;
-  fetch(`/api/pokemon?generation=${generation}`)
-    .then((response) => response.json())
-    .then((result) => {
-      resultDiv2.innerHTML = `<p>${result.data.join(", ")}</p>`;
-    })
-    .catch((error) => {
-      console.error("No se encontro la generacion");
-    });
-});
-
-searchButton3.addEventListener("click", () => {
-  const ability = abilityInput.value;
-  fetch(`/api/pokemon?ability=${ability}`)
-    .then((response) => response.json())
-    .then((result) => {
-      resultDiv3.innerHTML = `<p>${result.data.join(", ")}</p>`;
-    })
-    .catch((error) => {
-      console.error("No se encontro la habilidad");
-    });
-});
-
-resultDiv.addEventListener("click", () => {
-    
-})
-
-
-let pokemonCount = 0;
-let allPokemonHTML = '';
-
-async function preloadPokemon() {
-  await loadPokemon();
-}
-
-async function loadPokemon() {
-  const startIndex = pokemonCount + 1;
-  const endIndex = pokemonCount + 3;
-
-  for (let i = startIndex; i <= endIndex; i++) {
-    await fetch(`/api/pokemon/${i}`)
-      .then((response) => response.json())
-      .then((result) => {
-        allPokemonHTML += `
-                    <div class="pokemon-card">
-                        <p>${result.data.name}</p>
-                        <p>${result.data.types.map((type) => type.type.name).join(", ")}</p>
-                        <img src="${result.data.sprites.front_default}">
-                    </div>
-                `;
-      });
+// ----------------------------------------------------------------------------------------------------------------------------------------
+async function fetchPokemon(identifier) {
+  try {
+      const response = await fetch(`/api/pokemon/${identifier}`);
+      const data = await response.json();
+      
+      renderPokemon(data.data); //data.data porque el json de la api tiene un objeto data que contiene el pokemon (success y data)
+  } catch (error) {
+      console.error(error);
   }
-  pokemonCount = endIndex;
-  resultDiv.innerHTML = allPokemonHTML;
 }
+// ----------------------------------------------------------------------------------------------------------------------------------------
+async function renderPokemon(pokemon) {
+  allPokemonHTML.innerHTML += `
+      <div class="pokemon-card">
+          <p>${pokemon.name}</p>
+          <p>${pokemon.types.map((type) => type.type.name).join(", ")}</p>
+          <img src="${pokemon.sprites.front_default}">
+      </div>
+  `;
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------
+searchButton.addEventListener("click", () => {
+  allPokemonHTML.innerHTML = '';
+  if (pokemonInput.value === '') {
+    console.error("Please enter a pokemon name");
+    return;
+  }
+  fetchPokemon(pokemonInput.value);
+});
+// ----------------------------------------------------------------------------------------------------------------------------------------
+searchButton2.addEventListener("click", async () => {
+  
+  
+  const response = await fetch(`/api/pokemon?generation=${generationInput.value}`);
+  const data = await response.json();
+  
+  if (data.success === false || data === null || data === undefined) {
+      console.error("Generation not found");
+      return;
+  }
+  
+ data.data.forEach(pokemonName => fetchPokemon(pokemonName));
+});
 
-loadMoreButton.addEventListener("click", loadPokemon);
-preloadPokemon();
+// ----------------------------------------------------------------------------------------------------------------------------------------
+searchButton4.addEventListener("click", async () => {
+  
+  
+  const response = await fetch(`/api/pokemon?type=${typeInput.value}`);
+  const data = await response.json();
+  
+  if (data.success === false || data === null || data === undefined) {
+      console.error("Type not found");
+      return;
+  }
+ 
+ data.data.forEach(pokemonName => fetchPokemon(pokemonName));
+});
+
 
 
